de61f7e7cb6b69b3bee96bddb1f683ed216a96c0
djulia14 <djuliak10@gmail.com>
2026-09-21 21:25:29 -0300

    segundo commit

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ console.log('================================');
 console.log('       MEU APLICATIVO');
 console.log('================================');
 
-console.log('Nome: Lucas');
-console.log('Cidade: Guaramirim');
-console.log('Atividade de interesse: Jogos');
-console.log('Quero aprender: desenvolvimento de aplicativos');
-console.log('Área de interesse: tecnologia');
-console.log('Mensagem: Estou evoluindo na programação!');
+console.log('Nome: Djulai');
+console.log('Cidade: Schroeder');
+console.log('Atividade de interesse: volei');
+console.log('Quero aprender: sobre direito');
+console.log('Área de interesse: Direito');
+console.log('Mensagem: Estou estudando muito para ter um futuro bom e planejado!');
