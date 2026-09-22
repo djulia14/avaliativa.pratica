91e69280c53752a535724a077f3ea1a477593a1a
djulia14 <djuliak10@gmail.com>
2026-09-21 21:23:24 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log('================================');
+console.log('       MEU APLICATIVO');
+console.log('================================');
+
+console.log('Nome: Lucas');
+console.log('Cidade: Guaramirim');
+console.log('Atividade de interesse: Jogos');
+console.log('Quero aprender: desenvolvimento de aplicativos');
+console.log('Área de interesse: tecnologia');
+console.log('Mensagem: Estou evoluindo na programação!');
